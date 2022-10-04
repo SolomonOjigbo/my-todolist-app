5351ed5fd9abe67b8641f283041fc5f8682b3482
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-04 07:58:06 +0100

    Added interface Props for the input field and the add todo event handler function

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
-const InputField = () => {
+
+interface Props {
+	todo: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
 	return (
-		<form className="input">
-			<input type="input" placeholder="Enter a task" className="input__box" />
-			<button className="input__submit" type="submit">
-				Submit
-			</button>
-		</form>
+		<div>
+			<form
+				className="input"
+				onSubmit={(e) => {
+					handleAdd(e);
+					inputRef.current?.blur();
+				}}
+			>
+				<input
+					ref={inputRef}
+					type="input"
+					value={todo}
+					onChange={(e) => setTodo(e.target.value)}
+					placeholder="Enter a task"
+					className="input__box"
+				/>
+				<button className="input__submit" type="submit">
+					Submit
+				</button>
+			</form>
+		</div>
 	);
 };
 
